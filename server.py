aeaf2cc426a24b40858a6432e45d29fffc84da24
artur mf <gamesmf1009@gmail.com>
2026-05-21 01:57:33 -0300

    code base - server

--- server.py
+++ server.py
@@ -1,12 +1,22 @@
 import socket
+import threading
+import time
 
+PORT = 5050 #use high port numbers to avoid conflicts with well-known ports
+SERVER = #ip address of the server
+ADDR = (SEREVER, PORT) #create the pair that represent socket address on local network
 
-hostname = socket.gethostname()
-ip = socket.gethostbyname(hostname)
+#here we finally create the socket that is gonna handle the server connection
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #AF_INET indicates ipv4 and SOCK_STREAM indicates TCP
+server.bind(ADDR) #we define the address of the server socket
 
-print(f"ip: {ip}")
-print(f"hostname: {hostname}")
-print("testada pesada")
-soma = 10
-soma += 5
-print(f"soma: {soma}")
+def handle_client(conn, addr): #conn -> connection and addr -> address.
+    pass
+
+def start():
+    pass
+
+'''
+hostname = socket.gethostname() -> ask OS the name of current device
+ip = socket.gethostbyname(hostname) -> ask OS which is the ip addres of "hostname" device
+'''
