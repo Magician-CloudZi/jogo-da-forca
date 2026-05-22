69817074d1b9375d603b687af0ad5af5251b6c51
artur mf <gamesmf1009@gmail.com>
2026-05-21 21:33:19 -0300

    server and client review
    improved based on test files

--- client.py
+++ client.py
@@ -1,15 +1,19 @@
-
 import socket
+
+SERVER = "192.168.0.212"
+PORT = 5050
+FORMAT = "utf-8"
+
 #defini que vai ser ipv4 e tcp
 client  = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #iniciei a conexão com handshake
-client.connect("ip do melo", "mesma porta q ele ta usando")
+client.connect((SERVER, PORT))
 
 #enviando mensagem
-client.send("ney na copa fds". encode())
+mensagem = "ney na copa fds"
+client.send(mensagem.encode(FORMAT))
 #recebendo
-resposta = client.recv(1024).decode()
+resposta = client.recv(1024).decode(FORMAT)
 print(resposta)
 
 client.close()
-
--- server.py
+++ server.py
@@ -2,10 +2,9 @@ import socket
 import threading
 import time
 
-HEADER = 128 #how many bytes are gonna come
 PORT = 5050 #use high port numbers to avoid conflicts with well-known ports
-SERVER = "::" #socket.gethostbyname(socket.gethostname()) #ip address of the server. (My address in this case) #im using 0.0.0.0 to listen all networks interfaces that i have
-ADDR = (SERVER, PORT) #create the pair that represent socket address on local network
+HOST = "192.168.0.212" #socket.gethostbyname(socket.gethostname()) #ip address of the server. (My address in this case) #im using 0.0.0.0 to listen all networks interfaces that i have
+ADDR = (HOST, PORT) #create the pair that represent socket address on local network
 FORMAT = "utf-8" #std format that we gonna use to translate encode messages
 DISCONNECT_MESSAGE = "!DISCONNECT!" #default message to end connection
 
@@ -14,24 +13,26 @@ server = socket.socket(socket.AF_INET6, socket.SOCK_STREAM) #AF_INET indicates i
 server.bind(ADDR) #we define the address of the server socket
 
 def handle_client(conn, addr): #conn -> connection and addr -> address. // handle individual connections
-    print(f'[NEW CONNECTION || ] {addr} connected')
+    print(f'[NEW CONNECTION || ] {addr}')
 
     connected = True
     while connected:
-        msg_length = conn.recv(HEADER).decode(FORMAT) #find out which is the size on incoming message
-        msg_length = int(msg_length) #turns it into a integer, so we can use it
-        msg = conn.recv(msg_length).decode(FORMAT) #storage the package sent by client on "msg". This is a blocking line (program will sleep untill recieve it all)
-        if msg == DISCONNECT_MESSAGE:
+        data = conn.recv(1024).decode(FORMAT) #storage the package sent by client on "msg". This is a blocking line (program will sleep untill recieve it all)
+        msg = data.decode(FORMAT)
+        if not data or data == DISCONNECT_MESSAGE:
             connected = False
-    
-        print(f"[{addr}] sent -> {msg}")
 
-    conn.close()    
+        print(f"[{addr}] sent -> {msg}")
+        conn.send("[Message recivied]".encode(FORMAT))
+
+    conn.close()
+    print(f"[Disconnected] {addr}")
 
 
 def start(): #handle new connections
     server.listen() #server is now open to new connections
-    print(f"Server address: {SERVER}")
+    print(f"Server address: {HOST}:{PORT}")
+    
     while True:
         conn, addr = server.accept() #when a new connection occur, it will store the address and object(socket) that we can use to send files
         thread = threading.Thread(target=handle_client, args=(conn, addr))
