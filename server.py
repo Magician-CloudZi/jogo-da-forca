ab12f290a6d95bbd1ef00af1a2e546758636c7d7
cloudzi <cloudzi685@gmail.com>
2026-05-21 21:11:20 -0300

    Update server.py

--- server.py
+++ server.py
@@ -2,12 +2,15 @@ import socket
 import threading
 import time
 
+HEADER = 128 #how many bytes are gonna come
 PORT = 5050 #use high port numbers to avoid conflicts with well-known ports
-SERVER = #ip address of the server
-ADDR = (SEREVER, PORT) #create the pair that represent socket address on local network
+SERVER = "::" #socket.gethostbyname(socket.gethostname()) #ip address of the server. (My address in this case) #im using 0.0.0.0 to listen all networks interfaces that i have
+ADDR = (SERVER, PORT) #create the pair that represent socket address on local network
+FORMAT = "utf-8" #std format that we gonna use to translate encode messages
+DISCONNECT_MESSAGE = "!DISCONNECT!" #default message to end connection
 
 #here we finally create the socket that is gonna handle the server connection
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #AF_INET indicates ipv4 and SOCK_STREAM indicates TCP
+server = socket.socket(socket.AF_INET6, socket.SOCK_STREAM) #AF_INET indicates ipv4 and SOCK_STREAM indicates TCP
 server.bind(ADDR) #we define the address of the server socket
 
 def handle_client(conn, addr): #conn -> connection and addr -> address. // handle individual connections
@@ -15,16 +18,24 @@ def handle_client(conn, addr): #conn -> connection and addr -> address. // handl
 
     connected = True
     while connected:
-        msg = conn.recv() #storage the package sent by client on "msg". This is a blocking line (program will sleep untill recieve it all)
+        msg_length = conn.recv(HEADER).decode(FORMAT) #find out which is the size on incoming message
+        msg_length = int(msg_length) #turns it into a integer, so we can use it
+        msg = conn.recv(msg_length).decode(FORMAT) #storage the package sent by client on "msg". This is a blocking line (program will sleep untill recieve it all)
+        if msg == DISCONNECT_MESSAGE:
+            connected = False
+    
+        print(f"[{addr}] sent -> {msg}")
+
+    conn.close()    
+
 
 def start(): #handle new connections
     server.listen() #server is now open to new connections
+    print(f"Server address: {SERVER}")
     while True:
         conn, addr = server.accept() #when a new connection occur, it will store the address and object(socket) that we can use to send files
         thread = threading.Thread(target=handle_client, args=(conn, addr))
         thread.start()
 
-'''
-hostname = socket.gethostname() -> ask OS the name of current device
-ip = socket.gethostbyname(hostname) -> ask OS which is the ip addres of "hostname" device
-'''
+
+start()
